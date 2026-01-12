6490906bf3fd9e2de733bf6548d91f24ad703cfb
MOHAN P <152972970+MOHAN-MOORTHI@users.noreply.github.com>
2026-01-12 08:06:53 +0530

    Allow Cloudinary images by disabling Helmet CSP

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(compression()); // Compress all responses
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false })); // Allow external images (Cloudinary)
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
